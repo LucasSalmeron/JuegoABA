48d92e97ed8023f28ba662d526c28995dbf018e0
LucasSalmeron <salmeronlucas125@gmail.com>
2017-06-23 20:51:56 -0300

    agregado animacion de golpe y bloque invisible para colisionar con enemigo

--- Juego/source/Personaje.hx
+++ Juego/source/Personaje.hx
@@ -10,6 +10,9 @@ import flixel.FlxG;
 class Personaje extends FlxSprite 
 {
 
+	private var aux:Bool = false;
+	private var invisibleBox : FlxSprite;
+	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
@@ -18,9 +21,16 @@ class Personaje extends FlxSprite
 		
 		this.animation.add("caminar", [1,2,3,4,5,6],12);
 		this.animation.add("idle", [0]);
+		this.animation.add("golpe", [27, 28, 0], 10, false);
+		
 		this.animation.play("idle");
 		this.setGraphicSize(64, 128);
 		this.updateHitbox();
+		
+		
+		invisibleBox = new FlxSprite(0, 0);
+		invisibleBox.makeGraphic(20, 15);
+		FlxG.state.add(invisibleBox);
 	}
 	
 	override public function update(elapsed: Float){
@@ -31,6 +41,7 @@ class Personaje extends FlxSprite
 				this.animation.play("idle");
 			}
 		}
+		
 		if (FlxG.keys.pressed.LEFT && FlxG.keys.pressed.RIGHT){
 			if (this.animation.name == "caminar"){
 				this.animation.stop();
@@ -62,10 +73,24 @@ class Personaje extends FlxSprite
 			}
 		}
 		
-		
-		
-		
-		
+		if (FlxG.keys.justPressed.SPACE){
+			animation.play("golpe");
+			aux = true;
+		}
+		if (aux){
+			if (animation.name == "golpe" && animation.frameIndex == 28){
+				if (this.flipX){
+					invisibleBox.setPosition(x - invisibleBox.width, y + height / 2);
+				}
+				else
+				{
+					invisibleBox.setPosition(x + width, y + height/2);
+				}
+				aux = false;
+			}
+			
+		}
+	
 		
 	}
 }
